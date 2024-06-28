69dc902dd407b06b15eab6071846f3a3c0d5bcf7
Jim Cheung <usr.jim@gmail.com>
2024-06-28 09:55:44 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import createVuePlugin from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
-import fs from 'fs'
 
 const yearTime = 60 * 60 * 24 * 365 // 365 days
 
@@ -81,14 +80,6 @@ export default defineConfig({
     })
   ],
   server: {
-    port: 8080,
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['..']
-    },
-    https: {
-      key: fs.readFileSync('./.cert/key.pem'),
-      cert: fs.readFileSync('./.cert/cert.pem')
-    }
+    port: 8081
   }
 })
